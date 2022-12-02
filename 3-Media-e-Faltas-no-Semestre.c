be7bf3ed605213c6ad9a5db05ad22d8ab8daaf01
DanilloHildebrando <105883654+DanilloHildebrando@users.noreply.github.com>
2022-12-01 22:28:37 -0300

    Update and rename MediaeFaltaSemestre.c to 3-Media-e-Faltas-no-Semestre.c

--- 3-Media-e-Faltas-no-Semestre.c
+++ 3-Media-e-Faltas-no-Semestre.c
@@ -1,39 +1,35 @@
-/******************************************************************************
+// Este algoritmo calcula a média de notas de um aluno e considera sua quantidade de faltas no semestre
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
 
-*******************************************************************************/
 #include <stdio.h>
 
 int main()
 {
     float n1, n2, n3, med, medR;
     int fal;
+    
     printf("Informe as duas notas do aluno e a quantidade de faltas no semestre \n");
     scanf("%f %f %i", &n1, &n2, &fal);
-    
     
     if(fal > (6)){
         printf("O aluno reprovou por faltas");
     } else {
         med = (n1 + n2) / 2;
+        
         if(med >= (6)){
             printf("O aluno foi aprovado");
         } else {
             printf("Informe nota da prova de recuperação\n");
             scanf("%f", &n3);
             medR = (n1 + n2 + n3) / 3;
+            
             if(medR >= (6)){
-            printf("Aprovado no semestre");   
-        }else{
-            printf("Reprovado por nota");
-        }
-        
-    }
+                printf("Aprovado no semestre");   
+            } else {
+                printf("Reprovado por nota");
+        }        
+      }
     return 0;
-}
+   }
 }
 
