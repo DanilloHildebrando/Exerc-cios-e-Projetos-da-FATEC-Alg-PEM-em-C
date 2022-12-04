57d67a05581dfe486e5ca83c4f9ef31472511766
DanilloHildebrando <105883654+DanilloHildebrando@users.noreply.github.com>
2022-12-04 19:46:49 -0300

    Update and rename Cadastro.c to 9.9.8-Sistema-De-Cadastro.c

--- 9.9.8-Sistema-De-Cadastro.c
+++ 9.9.8-Sistema-De-Cadastro.c
@@ -1,8 +1,6 @@
 /******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
+// Este algorítmo faz o cadastro de clientes e verifica se o valor digitado para o CPF já foi cadastrado.
 
 *******************************************************************************/
 
