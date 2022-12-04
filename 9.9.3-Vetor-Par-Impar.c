96aea01085bac95705924cbcc5950536f1aff79e
DanilloHildebrando <105883654+DanilloHildebrando@users.noreply.github.com>
2022-12-04 19:31:07 -0300

    Update and rename A_vetorPar-Impar.c to 9.9.3-Vetor-Par-Impar.c

--- 9.9.3-Vetor-Par-Impar.c
+++ 9.9.3-Vetor-Par-Impar.c
@@ -1,9 +1,5 @@
 /******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
+// Este algoritmo veirifica dentro de um vetor os números pares e ímpares
 *******************************************************************************/
 
 #include <stdio.h>
