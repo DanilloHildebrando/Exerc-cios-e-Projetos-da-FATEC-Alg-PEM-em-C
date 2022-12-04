59e689e740e82910b63d32ac9d6b39907d6a9bf9
DanilloHildebrando <105883654+DanilloHildebrando@users.noreply.github.com>
2022-12-04 19:22:26 -0300

    Update and rename 3_Media-While.c to 9.8-Media-While.c

--- 9.8-Media-While.c
+++ 9.8-Media-While.c
@@ -1,14 +1,11 @@
 /******************************************************************************
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
+// Este algoritmo calcula a média de números informados até que se digite zero
 
 *******************************************************************************/
 
 #include <stdio.h>
 
-//3- Faça um laço que calcule a media de todos os números informados, a condição de termino do laço e quando for digitado ZERO
 
 
 int main()
@@ -18,9 +15,9 @@ int main()
     printf("Digite apenas números inteiros postivos e aperte zero para calcular a média: \n");
     while (n != 0)  {
        scanf("%i", &n);
-        if(n != 0) {
-        media += n;
-        cont ++;
+          if(n != 0) {
+            media += n;
+            cont ++;
               }
             }
 media = media / cont;
