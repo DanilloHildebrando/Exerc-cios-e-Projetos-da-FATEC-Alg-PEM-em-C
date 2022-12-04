d71a5f7fc185a5f1c86852f094607e10f856df16
DanilloHildebrando <105883654+DanilloHildebrando@users.noreply.github.com>
2022-12-04 19:04:43 -0300

    Update and rename VelocidadeVeiculo.c to 10-Velocidade-Veiculo-Multas.c

--- 10-Velocidade-Veiculo-Multas.c
+++ 10-Velocidade-Veiculo-Multas.c
@@ -1,10 +1,6 @@
 /******************************************************************************
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
+// Este algoritmo relaciona a velocidade de um carro com as restrições de avenidas e veririfca se houve multa e seu valor
 *******************************************************************************/
 #include <stdio.h>
 
@@ -22,21 +18,21 @@ int main()
     if (velC <= velA){
         printf("O veículo estava dentro da velocidade permitda");
     } else {
-           m1 = velA + 10;
+        m1 = velA + 10;
            if(velC <= m1){
                 printf("O veículo estava acima da velocidade e será multado em 50,00R$");    
-    } else {
+            } else {
                 m2 = velA + 30; 
-                if(velC <= m2){
-                    printf("O veículo estava acima da velocidade e será multado em 100,00R$"); 
-    } else {
-                    m3 = velA + 31;
-                    if(velC >= m3){
-                        printf("O veículo estava acima da velocidade e será multado em 200,00R$"); 
-    }
+                    if(velC <= m2){
+                        printf("O veículo estava acima da velocidade e será multado em 100,00R$"); 
+                    } else {
+                         m3 = velA + 31;
+                            if(velC >= m3){
+                                printf("O veículo estava acima da velocidade e será multado em 200,00R$"); 
+                            }
 
     return 0;
-}
-}
-}
+                    }
+            }
+           }
 }
