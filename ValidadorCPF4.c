a8f15f1dddaecddd4df4c5138ecd770a25b12beb
DanilloHildebrando <105883654+DanilloHildebrando@users.noreply.github.com>
2023-03-04 10:45:15 -0300

    Add files via upload

---
+++ ValidadorCPF4.c
@@ -0,0 +1,62 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include<stdio.h>
+#include<stdbool.h>
+
+void estado(int nonoDigito)
+{
+    char mat[10][100] =
+    {
+    "Rio Grande do Sul",
+    "Distrito Federal, Goiás, Mato Grosso, Mato Grosso do Sul e Tocantins",
+    "Amazonas, Pará, Roraima, Amapá, Acre e Rondônia",
+    "Ceará, Maranhão e Piauí",
+    "Paraíba, Pernambuco, Alagoas e Rio Grande do Norte",
+    "Bahia e Sergipe",
+    "Minas Gerais",
+    "Rio de Janeiro e Espírito Santo",
+    "São Paulo",
+    "Paraná e Santa Catarina"
+    };
+    printf("\n%s", mat[nonoDigito]);
+}
+
+
+bool repetidos(int cpf[])
+{
+    int x;
+    for(x=1; x<12; x++)
+    {
+    if(cpf[0] != cpf[x]) return false;
+    }
+        return true;
+}
+
+int calculaDigito(int cpf[], int mult[], int N)
+{
+int soma=0, i;
+for(i=0; i<N; i++) soma = soma + cpf[i]*mult[i];
+int resto = ((soma*10) % 11) % 10;
+return resto;
+}
+
+int main()
+{   int cpf[11];
+	int m1[9]={10,9,8,7,6,5,4,3,2};
+	int m2[10]={11,10,9,8,7,6,5,4,3,2};
+	int i;
+	printf("Entre com os 11 digitos do CPF:");
+	    for(i=0;i<11;i++) scanf("%d", &cpf[i]);
+	    
+	        if(calculaDigito(cpf, m1, 9) == cpf[9] && (calculaDigito(cpf, m2, 10) == cpf[10]) && repetidos(cpf) == false) printf("\nCPF válido.");
+	            else printf("\nCPF inválido");
+	            estado(cpf[8]);
+  	
+	return 0;
+}
