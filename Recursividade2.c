9fde56a5929899ffc9ab460a05b2e855de21a6bf
DanilloHildebrando <105883654+DanilloHildebrando@users.noreply.github.com>
2023-03-18 10:50:20 -0300

    Add files via upload

---
+++ Recursividade2.c
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+
+
+int alg1 (int n)
+
+{  if (n == 0) {
+    return 2;
+}
+   else {
+       int aux = alg1 (n - 1);
+       printf("%d - %d\n", n-1, aux);
+       return 2 * aux + 3;
+   }
+}
+
+
+int main()
+{
+    printf("%d", alg1(4));
+    
+}
